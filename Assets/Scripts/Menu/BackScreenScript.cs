03e474d0f853a0aa0fe93e01eaf7a58467f83e0c
Reveta <gfdsre1999@gmail.com>
2023-01-07 18:50:54 +0200

    Add Menu elements(in progress)

---
+++ Assets/Scripts/Menu/BackScreenScript.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class BackScreenScript : MonoBehaviour {
+	public List<Texture2D> images;
+	private RawImage _rawImage;
+
+	public int secondsToUpdate = 5;
+	void Start() {
+		_rawImage = gameObject.GetComponent<RawImage>();
+
+		StartCoroutine(UpdateScreen());
+	}
+
+	private void Update() {
+		
+
+	}
+
+	IEnumerator UpdateScreen() {
+		for (;;) {
+
+			var randomIndex = Random.Range(0, images.Count-1);
+			_rawImage.texture = images[randomIndex];
+			
+			yield return new WaitForSeconds(3);
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+	public void StartGame() {
+		
+	}
+
+	public void Exit() {
+		
+		print("ExitButton");
+		Application.Quit();
+	}
+}
